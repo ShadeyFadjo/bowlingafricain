4021280e7f35ae3c8d99c2b2a1b23072683b7bc8
ShadeyFadjo <114293328+ShadeyFadjo@users.noreply.github.com>
2024-12-14 12:42:20 +0300

    Init and Start coding

--- accueil.py
+++ accueil.py
@@ -5,13 +5,12 @@ from urllib.parse import unquote
 
 app = Flask(__name__)
 
-
 @app.route("/")
 def index():
-    return "<p>Accueil, Kal!</p>"
+    return render_template('accueil.html')
 
-@app.route('/accueil/<pays>')
-def kal(pays=None):
-    nompays=escape(pays.replace('_',' '))
-    return render_template('accueil.html',nompays=nompays)
+# @app.route('/accueil/<pays>')
+# def kal(pays=None):
+#     nompays=escape(pays.replace('_',' '))
+#     return render_template('accueil.html',nompays=nompays)
 
